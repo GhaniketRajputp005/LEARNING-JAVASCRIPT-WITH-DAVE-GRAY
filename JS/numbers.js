da21875fcd385988df990fb59b6111159d1801d5
Ghaniket Rajput <ghaniketrajput29@gmail.com>
2024-03-28 00:26:06 +0530

    L4 Number methods

---
+++ JS/numbers.js
@@ -0,0 +1,21 @@
+const myNum = 43;
+const myFloat = 43.01;
+const string = "df34df";
+
+console.log(myNum);
+console.log(myFloat);
+console.log(myNum == myFloat);
+console.log(Number(string) + 4);
+console.log(Number("hello") );
+console.log(Number(undefined));
+console.log(Number(true));
+
+console.log(Number.isInteger(myNum));
+console.log(Number.parseFloat(string));
+console.log(myFloat.toFixed(1));
+
+// The Number.isNaN() method determines whether the passed value is NaN and its type is Number.
+
+// The global isNaN() function determines whether a value is NaN or not.
+
+console.log(isNaN("dave"));
