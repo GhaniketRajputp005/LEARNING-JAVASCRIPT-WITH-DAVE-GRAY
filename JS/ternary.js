aeec7ffb5eb932930917066fbffa6f3af3b682b4
Ghaniket Rajput <ghaniketrajput29@gmail.com>
2024-03-28 11:43:28 +0530

    L9 ternary_operator

---
+++ JS/ternary.js
@@ -0,0 +1,13 @@
+// let soup= "he";
+
+// let moup = soup? "yay ${soup}":"no";
+
+let testScore = 79;
+let myGrade = testScore > 89 ? " A"
+:testScore > 79 ? "B"
+:testScore > 69 ? "C"
+:testScore > 59 ? "D"
+:testScore > 49 ? "E"
+:"F";
+
+console.log(`my test grade is  ${testScore}`);
